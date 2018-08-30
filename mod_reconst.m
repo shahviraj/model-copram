9d3f51f14e5fbb4cc1c32f700c3b6530c24b06ac
shahviraj <viraj@iastate.edu>
2018-08-30 03:14:46 -0500

    updates

--- mod_reconst.m
+++ mod_reconst.m
@@ -9,7 +9,7 @@ pr.b = 1; %number of blocks if signal is block-sparse; otherwise keep 1
 pr.tol1 = 1e-5; %error tolerance for measurements
 pr.tol2 = 1e-7;
 pr.max_iter = 15;
-pr.R = 2; %period of the modulo function
+pr.R = 1; %period of the modulo function
 pr.rho = 3;%spread of the true measurements, y =A*z
 pr.del = 1; %truncation factor for supp estimation
 pr.spgl_opts = spgSetParms('verbosity',0);
@@ -19,7 +19,7 @@ pr.spgl_opts = spgSetParms('verbosity',0);
 %pr.mspan=[pr.mspan1,pr.mspan2];
 pr.mspan=100:100:1000;
 pr.num_trials = 10;
-pr.s_span = 3:3:9; % sparsity
+pr.s_span = 3:3:12; % sparsity
 pr.amp = 1; %amplification factor 
 pr.del_p = 0.005; % ps = del*m (sparsity pertaining to error in p)
 pr.method = 'justice-pursuit';
